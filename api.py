99354e16e6e0b6f3cdec75474f71a6eb219c7dde
fedeclerici-oss <fedeclerici@gmail.com>
2026-02-11 11:38:15 -0300

    Update api.py

--- api.py
+++ api.py
@@ -178,10 +178,10 @@ def bot(
         }
 
     # SIN UBICACIÓN → lista útil
-    if lat is None or lon is None:
+       if lat is None or lon is None:
         cantidad = len(candidatos)
 
-    if cantidad == 1:
+        if cantidad == 1:
             texto = f"Encontré 1 {categoria}."
         else:
             texto = f"Encontré {cantidad} {categoria}s."
@@ -190,6 +190,7 @@ def bot(
             "respuesta": f"{texto} Si me compartís tu ubicación, te digo cuál es el más cercano.",
             "datos": candidatos[:5],
         }
+
 
 
 
@@ -286,5 +287,6 @@ async def telegram_webhook(request: Request):
         )
 
     return {"ok": True}
+
 
 
